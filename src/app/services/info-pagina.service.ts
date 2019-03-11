c6eac5dd26ade4aaa2c8be89a655d8764e618d7c
Ana Betancourt <anarocio26@gmail.com>
2019-03-10 20:59:15 -0600

    final de la seccion 5

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -8,16 +8,25 @@ import { InfoPagina } from '../interfaces/info-pagina.interface';
 export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
+  equipo: any[] = [];
 
     // Leer archivo JSON
   constructor(private http: HttpClient) {
-
+      this.cargarInfo();
+      this.cargarEquipo();
+  }
+   private cargarInfo() {
     this.http.get('assets/data/data-pagina.json')
     .subscribe((resp: InfoPagina) => {
       this.cargada = true;
       this.info = resp;
-      console.log(resp);
     });
-
+   }
+   private cargarEquipo() {
+     this.http.get('https://angular-html-ana.firebaseio.com/equipo.json')
+     .subscribe((resp: any[]) => {
+       this.equipo = resp;
+       console.log(resp);
+     });
    }
   }
